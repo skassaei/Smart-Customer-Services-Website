e86375f7481758666fc1edfa5d863b806e5a7d33
skassaei <skassaei@torontomu.com>
2023-04-07 14:22:43 -0400

    .

--- contactus.php
+++ contactus.php
@@ -23,10 +23,10 @@ if (isset($_SESSION['loggedin'])) {
             <div class="card col-6 bg-darkk mainc contC text-center" style="padding: 5vmin">
             <h2 class="pb-4 ">HOW TO CONTACT US</h2>
             <h3>EMAILS:</h3>
-            <p>Parmida Azizi: </p>
-            <p>Sahar Kassaei: </p>
-            <p>Alex Huynh: </p>
-            <p>Sina Pahlavan: </p>
+            <p>Parmida Azizi: parmida.azizi@torontomu.ca </p>
+            <p>Sahar Kassaei: skassaei@torontomu.ca </p>
+            <p>Alex Huynh: a10huynh@torontomu.ca</p>
+            <p>Sina Pahlavan: spahlavan@torontomu.ca</p>
             <h3 class="pt-3 ">PHONES:</h3>
             <p>Parmida Azizi: </p>
             <p>Sahar Kassaei: </p>
--- nav.php
+++ nav.php
@@ -18,7 +18,6 @@
           <li class="nav-item dropdown">
 			<a class="nav-link active" href="discount.php">Special Offers</a>
 		  </li>
-      <li><a href="findOrder.php" style="text-decoration:none; color:white;">ORDERS</a>
 
 		  <?php 
 			if(isset($_SESSION['isAdmin'])) {
@@ -45,6 +44,7 @@
 		if (isset($_SESSION['loggedin'])) {
 			?>
 			<ul class="nav navbar-nav navbar-right light">
+      <li><a href="findOrder.php" style="text-decoration:none; color:white;">ORDERS</a>
             <li><a href="#"><svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5.60464 11.4417C5.79269 13.1343 5.88672 13.9805 6.45624 14.4903C7.02576 15 7.87723 15 9.58017 15H9.70588H13.4706H14.827C16.0002 15 16.5867 15 17.0627 14.7134C17.5387 14.4268 17.8132 13.9084 18.3621 12.8716L20.8303 8.20947C21.361 7.207 20.6343 6 19.5 6V6H9.70588H9.46906C7.38336 6 6.34051 6 5.74427 6.66616C5.14803 7.33231 5.2632 8.36878 5.49353 10.4417L5.60464 11.4417Z" stroke="#f2f2f2" stroke-width="1" stroke-linejoin="round"></path> <path d="M3 3H3.5V3C4.1642 3 4.72218 3.49942 4.79553 4.15955L5.55558 11" stroke="#f2f2f2" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M9 19.5C9 20.3284 8.32843 21 7.5 21C6.67157 21 6 20.3284 6 19.5C6 18.6716 6.67157 18 7.5 18C8.32843 18 9 18.6716 9 19.5Z" stroke="#f2f2f2" stroke-width="1"></path> <path d="M18 19.5C18 20.3284 17.3284 21 16.5 21C15.6716 21 15 20.3284 15 19.5C15 18.6716 15.6716 18 16.5 18C17.3284 18 18 18.6716 18 19.5Z" stroke="#f2f2f2" stroke-width="1"></path> </g></svg></a></li>
             <li><a href="DB_Operations/logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-closed" viewBox="0 0 16 16">
   <path d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2zm1 13h8V2H4v13z"/>
